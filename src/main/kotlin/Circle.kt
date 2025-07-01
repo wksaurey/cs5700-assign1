0e2e1bda3d46c75c1be5e60d20eb8df66c47b45a
Kolter Saurey <wksaurey@gmail.com>
2025-07-01 14:29:04 -0600

    finished circle

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,6 +1,11 @@
+import kotlin.math.PI
+import kotlin.math.pow
+
 class Circle(
     point: Point,
     radius: Double
 ): Square(point, radius*2) {
     var radius: Double = validateValue(radius)
+
+    override fun getArea() = 2*PI*radius.pow(2)
 }
